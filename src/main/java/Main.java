3dfad160ffe6e2a76ea2456a6508ea3b236cc9cd
irinababunova <xleyla@gmail.com>
2023-10-29 13:53:50 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final int apartmentPrice = 16500000;
+        System.out.println("Цена квартиры - " + apartmentPrice);
+
+        final double percentFirstPayment = 0.2;
+        System.out.println("Процент первоначального взноса - " + percentFirstPayment);
+
+        double amountFirstPayment = apartmentPrice * percentFirstPayment;
+        System.out.println("Сумма первоначального взноса - " + amountFirstPayment);
+    }
+}
